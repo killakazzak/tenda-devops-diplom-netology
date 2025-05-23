37a87040b488541bb3ebcce35fa4762f415a0851
Denis Ten <denis.a.ten@gmail.com>
2025-05-23 06:53:23 +0000

    new file:   yc-main-infra/dns.tf
    	new file:   yc-main-infra/dns.tf

---
+++ yc-main-infra/dns.tf
@@ -0,0 +1,31 @@
+# Создаем DNS-зону
+resource "yandex_dns_zone" "denisten_ru" {
+  name        = "denisten-ru"
+  description = "Main DNS zone"
+  zone        = "denisten.ru."
+  folder_id   = "b1g3hhpc4sj7fmtmdccu"
+  public      = true
+}
+
+# Получаем информацию о балансировщике по его имени
+data "yandex_lb_network_load_balancer" "lb-ingress" {
+  name = "lb-ingress" # Указываем имя балансировщика
+}
+
+resource "yandex_dns_recordset" "denisten_ru_a" {
+  zone_id = yandex_dns_zone.denisten_ru.id
+  name    = "denisten.ru."
+  type    = "A"
+  ttl     = 600
+  data = flatten([
+    for listener in data.yandex_lb_network_load_balancer.lb-ingress.listener :
+    [for address in listener.external_address_spec : address.address]
+  ])
+}
+
+output "lb_listener" {
+  value = flatten([
+    for listener in data.yandex_lb_network_load_balancer.lb-ingress.listener :
+    [for address in listener.external_address_spec : address.address]
+  ])
+}
